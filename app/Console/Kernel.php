03d94025fb3f84d01e55c8d356267dd6b5368b3c
shaktirajalgopage <shaktirajalgopage@gmail.com>
2026-06-18 17:36:31 +0530

    12.00 am  condition added nhere

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('employees:check-birthdays')->dailyAt('17:12');
+        $schedule->command('employees:check-birthdays')->dailyAt('00:12');
     }
 
     /**
@@ -21,6 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
+        
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
